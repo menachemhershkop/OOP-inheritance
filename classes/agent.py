baa33c9004d9ad341927ea315c05fcb9d1e1338b
mendy hershkop <hers.mm@gmail.com>
2025-11-03 12:04:54 +0200

    create Agent class

--- classes/agent.py
+++ classes/agent.py
@@ -1,6 +1,7 @@
 class Agent:
-    def __init__(self,code_name, clearance_level):
-        self.code_name=str(code_name)
-        self.clearance_level=int(clearance_level)
+    '''Agent code, and clearance level'''
+    def __init__(self,code_name:str, clearance_level:int):
+        self.code_name=code_name
+        self.clearance_level=clearance_level
     def report(self):
         print(f'Agent {self.code_name} reporting. Clearance Level: {self.clearance_level}')
